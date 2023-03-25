bb0d3f0c4c3516f3d330fcc62f7d98529ef9d6f2
Ferran Sánchez <fersandom@gmail.com>
2023-03-25 13:10:47 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import axios from "axios";
+
+console.log(import.meta.env.VITE_API_KEY);
+async function fetchApple() {
+  let dataStock;
+  await axios.get(`https://finnhub.io/api/v1/stock/candle?symbol=IBML&resolution=D&count=500&token=${import.meta.env.VITE_API_KEY}`)
+    .then(response => { dataStock = response.data });
+  console.log(dataStock);
+}
+
+fetchApple();
+</script>
+
+<template>
+  <h1>Hola</h1>
+</template>
+
+<style scoped></style>
